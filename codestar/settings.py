f0c218abc62f79bd6f2fde2561cff587bce82620
Maya-Claveau <maya.claveau@gmail.com>
2022-06-13 16:40:42 +0000

    add a heroku as llowed host t

--- codestar/settings.py
+++ codestar/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["codestar062022.herokuapp.com", "localhost"]
 
 # Application definition
 
