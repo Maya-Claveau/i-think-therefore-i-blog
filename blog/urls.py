0d817a10788400a28f74e3e05489ccb6d793fd3a
Maya-Claveau <maya.claveau@gmail.com>
2022-06-14 08:49:00 +0000

    Fix the typo in settings.py for static file path

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,9 @@
-from . import views
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+from . import views
 
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
